8064f36e470e5c38370f571c0d2957d881de661d
David <46662617+Osgoroth@users.noreply.github.com>
2023-08-20 19:41:19 +0100

    editing fullscreen to follow docs

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,17 +1,15 @@
+// grab the elements
 const player = document.querySelector(".player");
 const video = document.querySelector(".viewer");
-
 const progress = document.querySelector(".progress");
 const progressBar = document.querySelector(".progress__filled");
-
 const playButton = document.querySelector(`[title="Toggle Play"]`);
 const rangeButtons = document.querySelectorAll(`[type="range"]`);
 const fullscreenButton = document.querySelector(`[name="fullscreen"]`);
-
 const skipButtons = document.querySelectorAll(`[data-skip]`);
-
 const controls = document.querySelector(".player__controls");
 
+// make some functions to do stuff
 function skip() {
   //   console.log("skipped");
   video.currentTime += parseFloat(this.dataset.skip);
@@ -53,40 +51,31 @@ function scrub(e) {
 function handleFullscreen() {
   if (!document.fullscreenElement) {
     player.requestFullscreen();
-  } else if (document.fullscreenElement) {
+  } else if (document.exitFullscreen) {
     document.exitFullscreen();
     console.log("exiting fullscreen");
   }
 }
 
+//wire the elements to the functions and listen for clicks etc
+// video events
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 video.addEventListener("timeupdate", handleProgress);
-
+//player controls events
 playButton.addEventListener("click", togglePlay);
-
+skipButtons.forEach((button) => button.addEventListener("click", skip));
 rangeButtons.forEach((range) =>
   range.addEventListener("change", handleRangeUpdate)
 );
 rangeButtons.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
-
 fullscreenButton.addEventListener("click", handleFullscreen);
-
+// progress bar events
 let mousedown = false;
 progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
-
-skipButtons.forEach((button) => button.addEventListener("click", skip));
-
-// video.addEventListener("fullscreenchange", () => {
-//   if (document.fullscreenElement) {
-//     controls.style.display = "block";
-//   } else {
-//     controls.style.display = "flex";
-//   }
-// });
