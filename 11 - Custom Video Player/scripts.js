cd57a82d789ba4536c507ab15dfcba55b339405a
David <46662617+Osgoroth@users.noreply.github.com>
2023-08-20 19:34:50 +0100

    implemented fullscreen

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,13 +1,19 @@
-const video = document.querySelector("video");
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+
+const progress = document.querySelector(".progress");
 const progressBar = document.querySelector(".progress__filled");
 
 const playButton = document.querySelector(`[title="Toggle Play"]`);
 const rangeButtons = document.querySelectorAll(`[type="range"]`);
+const fullscreenButton = document.querySelector(`[name="fullscreen"]`);
 
 const skipButtons = document.querySelectorAll(`[data-skip]`);
 
+const controls = document.querySelector(".player__controls");
+
 function skip() {
-  console.log("skipped");
+  //   console.log("skipped");
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
@@ -20,24 +26,37 @@ function togglePlay() {
 }
 
 function handleRangeUpdate() {
-  //   console.log(this.name);
   video[this.name] = this.value;
 }
-// this gets called even if the video isnt playing...
-// setInterval(() => {
-//   let currentPercent = Math.floor((video.currentTime / video.duration) * 100);
-//   // set the width of the progress bar (flex-basis)
-//   progressBar.style["flex-basis"] = `${currentPercent}%`;
-//   //   console.log(currentPercent);
-// }, 1000);
+
 function handleProgress() {
-  const currentPercent = Math.floor((video.currentTime / video.duration) * 100);
+  const currentPercent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${currentPercent}%`;
 }
 
 function updateButton() {
   const icon = this.paused ? "►" : "❚ ❚";
   playButton.textContent = icon;
+}
+
+function scrub(e) {
+  //firefox shows offsetX as 0..but logging it to console gives a value
+  // this is 100%
+  console.log(progress.offsetWidth);
+  //this is current percentage
+  console.log(e.offsetX);
+  scrubbedTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  console.log(scrubbedTime);
+  video.currentTime = scrubbedTime;
+}
+
+function handleFullscreen() {
+  if (!document.fullscreenElement) {
+    player.requestFullscreen();
+  } else if (document.fullscreenElement) {
+    document.exitFullscreen();
+    console.log("exiting fullscreen");
+  }
 }
 
 video.addEventListener("click", togglePlay);
@@ -50,8 +69,24 @@ playButton.addEventListener("click", togglePlay);
 rangeButtons.forEach((range) =>
   range.addEventListener("change", handleRangeUpdate)
 );
+rangeButtons.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+fullscreenButton.addEventListener("click", handleFullscreen);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
 
 skipButtons.forEach((button) => button.addEventListener("click", skip));
 
-// skip10.addEventListener("click", skipBack);
-// skip25.addEventListener("click", skipForward);
+// video.addEventListener("fullscreenchange", () => {
+//   if (document.fullscreenElement) {
+//     controls.style.display = "block";
+//   } else {
+//     controls.style.display = "flex";
+//   }
+// });
